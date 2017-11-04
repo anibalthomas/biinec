b327820faa30278df7e474ebd903056fa0df9f7b
Anibal <jovomilla@gmail.com>
2017-11-03 18:04:37 -0600

    carga

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -97,17 +97,17 @@
 
 
            <div class="col-lg-3 col-xs-6">
-             <!-- small box -->
+
              <div class="small-box bg-red">
                <div class="inner">
-                 <h3>{{$descargas}}</h3>
+                 <h3>0</h3>
 
                  <p>Imagenes descargadas</p>
                </div>
                <div class="icon">
                  <i class="ion ion-ios-cloud-download"></i>
                </div>
-               {{-- <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a> --}}
+
              </div>
            </div>
               @endif
